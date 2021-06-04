b75f21d83d974ae017c116c5c1a40721de28490e
gilxa1226 <mike.haller@gmail.com>
2021-06-04 13:42:03 -0400

    cleanup for beta2

--- src/main/java/us/mjhtech/liveproject/cakefactory/catalog/CatalogService.java
+++
@@ -1,7 +0,0 @@
-package us.mjhtech.liveproject.cakefactory.catalog;
-
-public interface CatalogService {
-
-    Iterable<PastryItem> getPastryItems();
-
-}
--- src/main/java/us/mjhtech/liveproject/cakefactory/catalog/persistence/PastryItemRepository.java
+++
@@ -1,6 +0,0 @@
-package us.mjhtech.liveproject.cakefactory.catalog.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PastryItemRepository extends CrudRepository<PastryItemEntity, String> {
-}
